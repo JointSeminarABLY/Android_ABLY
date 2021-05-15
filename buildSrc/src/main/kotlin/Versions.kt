d241107ce44ffb93cabdd3018ed044ddf3e7970a
SONPYEONGHWA <ssrsph@naver.com>
2021-05-15 16:13:42 +0900

    chore: add dependencies

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,27 @@
+object Versions {
+    const val kotlin_version = "1.4.3"
+
+    const val retrofit_version = "2.9.0"
+    const val okhttp_version = "4.9.0"
+
+    const val rxjava_version = "2.1.9"
+    const val rxandroid_version = "2.0.2"
+
+    const val room_version = "2.3.0-alpha04"
+
+    const val hilt_version = "2.32-alpha"
+
+    const val paging_version = "3.0.0-alpha13"
+
+    const val glide_version = "4.12.0"
+    const val coil_version = "1.1.1"
+
+    const val navigation_version = "2.3.5"
+    const val fragment_ktx_version = "1.3.1"
+    const val activity_ktx_version = "1.2.1"
+    const val lifecycle_version = "2.2.0"
+
+    const val lorem_version = "2.1"
+
+    const val annotations_version = "28.0.0"
+}
